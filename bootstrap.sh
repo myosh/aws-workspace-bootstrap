9b7bf44847a6ddea160d71bf4b576b204edf086b
Marco Dörfliger <marco.dorfliger@myosh.com>
2025-05-20 13:00:36 +0200

    added context for newbie users

--- bootstrap.sh
+++ bootstrap.sh
@@ -73,6 +73,7 @@ fi
 # --- 7. Run the Ansible Playbook ---
 cd "$SETUP_DIR/ansible"
 echo "🚀 Running the Ansible playbook..."
+echo "(note: \"BECOME password\" just means your own AWS workspace password)"
 ansible-playbook -i inventory.ini playbook.yml --ask-become-pass
 
 echo ""
